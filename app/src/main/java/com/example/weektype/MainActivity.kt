a62f623791f86f09d2beb09825fc060a78916251
Nikita Titov <titov.amm.vsu@gmail.com>
2021-10-19 19:46:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/weektype/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.weektype
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.Month
+import android.view.View
+
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val endDate = LocalDate.now()
+        val startDate = LocalDate.of(endDate.year, Month.SEPTEMBER, 1).with(DayOfWeek.MONDAY)
+        val weeksDiff = 5
+        val weekType: String = if (weeksDiff % 2 == 0) {
+            "Числитель"
+        } else {
+            "Знаменатель"
+        }
+
+        setContentView(R.layout.activity_main)
+        val weekTypeView = findViewById<View>(R.id.weekType) as TextView
+        weekTypeView.text = weekType
+    }
+}
